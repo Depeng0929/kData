a2278f9987717b2ace2123798bec582773901fdf
depeng.kong <15365182986@163.com>
2020-03-12 19:16:29 +0800

    test: queue test

---
+++ test/core/queue/deque.test.ts
@@ -0,0 +1,33 @@
+import { Deque } from '../../../src/core/queue/deque'
+
+describe('deque is ok', function() {
+  const deque = new Deque()
+  beforeEach(function() {
+    deque.addBack(1)
+    deque.addBack(2)
+    deque.addBack(3)
+    deque.addBack(4)
+  })
+  afterEach(function() {
+    deque.clear()
+  })
+
+  test('addBack and clear is ok', function() {
+    expect(deque.size).toBe(4)
+  })
+  test('removeBack and peekBack is ok', function() {
+    const item = deque.removeBack()
+    expect(item).toBe(4)
+    expect(deque.peekBack()).toBe(3)
+    expect(deque.size).toBe(3)
+  })
+  test('addFront and peekFront is ok', function() {
+    deque.addFront(0)
+    expect(deque.peekFront()).toBe(0)
+  })
+  test('removeFront is ok?', function() {
+    const item = deque.removeFront()
+    expect(item).toBe(1)
+    expect(deque.size).toBe(3)
+  })
+})
