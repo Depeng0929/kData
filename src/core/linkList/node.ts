4e60a43f1122ffcb1db937d22f4a483dbf157040
depeng.kong <15365182986@163.com>
2020-03-14 15:28:48 +0800

    feat(linkList): add doublyNode

--- src/core/linkList/node.ts
+++ src/core/linkList/node.ts
@@ -1,16 +1,18 @@
 export class Node {
-  public value: any;
-  public next: Node | undefined;
+  public value: any
+  public next: Node | undefined
   constructor(value: any) {
-    this.value = value;
-    this.next = undefined;
+    this.value = value
+    this.next = undefined
   }
 }
 
-export class DoublyNode extends Node{
-  public prev: DoublyNode | undefined;
-  constructor(element: any ) {
-    super(element);
-    this.prev = undefined;
+export class DoublyNode extends Node {
+  public prev: DoublyNode | undefined
+  public next: DoublyNode | undefined
+  constructor(element: any) {
+    super(element)
+    this.prev = undefined
+    this.next = undefined
   }
 }
