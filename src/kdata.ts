06532c0589c5df3d85f30c17fc6f98ba259352bf
depeng.kong <15365182986@163.com>
2020-03-14 15:35:06 +0800

    style: commit lint

--- src/kdata.ts
+++ src/kdata.ts
@@ -1,8 +1,8 @@
-import { Stack } from './core/stack'
-import { Queue } from './core/queue'
-import { Deque } from './core/queue/deque'
-import { LinkList } from './core/linkList'
-import { DoublyLinkList } from './core/linkList/doublyLinkList'
-import { CircularLinkList } from './core/linkList/circularLinkList'
+import { Stack } from './core/stack';
+import { Queue } from './core/queue';
+import { Deque } from './core/queue/deque';
+import { LinkList } from './core/linkList';
+import { DoublyLinkList } from './core/linkList/doublyLinkList';
+import { CircularLinkList } from './core/linkList/circularLinkList';
 
-export { Stack, Queue, Deque, LinkList, DoublyLinkList, CircularLinkList }
+export { Stack, Queue, Deque, LinkList, DoublyLinkList, CircularLinkList };
